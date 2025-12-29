03761255bd10f7d13e0c62af47f0126910feb274
BharathKumar03044 <kumarbharath560@gmail.com>
2025-12-29 14:18:04 +0530

    generated new code

---
+++ .vs/SQL-handson-RCG/v15/# This is a simple Python program that.py
@@ -0,0 +1,9 @@
+# This is a simple Python program that prints "Hello, World!"
+
+def main(pratap):
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main(girish)
+
+    to check and for sake practisegit commands
